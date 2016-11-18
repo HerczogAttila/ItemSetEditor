1cb02741f4ed4e5ae674b652774e19be39bb2e6c
Herczog Attila <herczogattil@gmail.com>
2016-11-18 19:30:23 +0100

    Rename project

--- ItemSetEditor/App.xaml.cs
+++ ItemSetEditor/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ItemSetMaker
+namespace ItemSetEditor
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- ItemSetEditor/Properties/AssemblyInfo.cs
+++ ItemSetEditor/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ItemSetMaker")]
+[assembly: AssemblyTitle("ItemSetEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ItemSetMaker")]
+[assembly: AssemblyProduct("ItemSetEditor")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
